cca7f0bd25eeffb247b3f94297f621fbb4df6d1d
Emabliss <emmajmv@gmail.com>
2021-11-30 22:59:53 +0000

    new commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export const getStaticProps: GetStaticProps = async () => {
   return { props: { feed } };
 };
 
-const Blog: React.FC<PostProps> = (props) => {
+const Blog: React.FC<props> = (props) => {
   
   return (
     <Layout>

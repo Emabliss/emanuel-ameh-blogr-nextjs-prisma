88067270e2caa5daa6cd62623e59c970459291e3
Emabliss <emmajmv@gmail.com>
2021-11-30 22:31:48 +0000

    new commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,22 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-
-let prisma: PrismaClient;
+import { PrismaClient } from '@prisma/client'
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: any;
-    }
-  }
+  var prisma: PrismaClient | undefined
 }
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
 export default prisma;
